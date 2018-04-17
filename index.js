85f2395d58c66fc3f4b96be53debeaee7ccbe031
Trishla Verma <trishla.verma@gmail.com>
2018-04-17 15:30:22 +0530

    sql connected

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+
+  host: "localhost",
+
+  user: "root",
+
+  password: "",
+
+  database: "mydb"
+
+});
 /*
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
@@ -32,6 +45,10 @@ app.get('/', function(req, res){
     res.sendFile(path.join(__dirname+'/index.html'));
 })
 
+app.get('/personalmenu', function(req, res){
+    res.sendFile(path.join(__dirname+'/personalmenu.html'));
+})
+
 app.get('/submit', function(req,res){
     console.log("hello");
     res.sendFile(path.join(__dirname+'/signup.html'));
@@ -43,11 +60,47 @@ app.post('/submit', function(req, res){
     var email=req.body.email;
 
     var username=req.body.username;
+
+    var pass=req.body.pass;
     //Prints Name
     console.log("hello" + name);
     
     //Redirect to homepage
     res.redirect('/');
+
+    con.connect(function(err) {
+
+      if (err) throw err;
+
+      var sql = "INSERT INTO users (name, email,username,password) VALUES ('"+name+"', '"+email+"','"+username+"','"+pass+"')";
+
+      con.query(sql, function (err, result) {
+
+        if (err) throw err;
+
+        console.log("1 record inserted");
+
+         res.end();
+    });
+    });
+
+})
+
+app.get('/loginsubmit', function(req,res){
+    console.log("hello");
+    res.sendFile(path.join(__dirname+'/login.html'));
+})
+
+app.post('/loginsubmit', function(req, res){
+
+    var user=req.body.user;
+
+    var pass=req.body.pass;
+    //Prints Name
+    console.log("password is " + pass);
+    
+    //Redirect to homepage
+    res.redirect('/personalmenu.html');
 
 })
 
